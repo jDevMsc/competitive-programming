f40673003e5c55dedb823c61576eb5f255b0ee72
Artem Luzan <unlimitet2011@yandex.ru>
2019-07-13 21:51:12 +0300

    CreatePomProject

--- collections/src/main/java/ru/compprog/linkedlist/LinkedList.java
+++ collections/src/main/java/ru/compprog/linkedlist/LinkedList.java
@@ -1,59 +1,66 @@
 package ru.compprog.linkedlist;
-import java.util.*;
+
+
+import java.util.ArrayList;
 
 public class LinkedList {
-   public Node head;
-   public Node tail;
 
-   public LinkedList() {
-      head = null;
-      tail = null;
-   }
+  public Node head;
+  public Node tail;
 
-   public void addInTail(Node item) {
-      if (this.head == null) {
-         this.head = item;
-      } else {
-         this.tail.next = item;
+  public LinkedList() {
+    head = null;
+    tail = null;
+  }
+
+  public void addInTail(Node item) {
+    if (this.head == null) {
+      this.head = item;
+    } else {
+      this.tail.next = item;
+    }
+    this.tail = item;
+  }
+
+  public Node find(int value) {
+    Node node = this.head;
+    while (node != null) {
+      if (node.value == value) {
+        return node;
       }
-      this.tail = item;
-   }
+      node = node.next;
+    }
+    return null;
+  }
 
-   public Node find(int value) {
-      Node node = this.head;
-      while (node != null) {
-         if (node.value == value) {
-            return node;
-         }
-         node = node.next;
+  public ArrayList<Node> findAll(int value) {
+    ArrayList<Node> nodes = new ArrayList<Node>();
+    return nodes;
+  }
+
+  public boolean remove(int value) {
+    Node node = this.head;
+    while (node != null) {
+      if (node.value == value) {
+        return true;
       }
-      return null;
-   }
+      node = node.next;
+    }
 
-   public ArrayList<Node> findAll(int value) {
-      ArrayList<Node> nodes = new ArrayList<Node>();
-      return nodes;
-   }
-
-   public boolean remove(int value) {
-      Node node = this.head;
-      while (node != null) {
-         if (node.value == value) {
-            return true;
-         }
-         node = node.next;
-      }
-
-      return false; // если узел был удалён
-   }
+    return false; // если узел был удалён
+  }
 }
 
-class Node
-{
-   public int value;
-   public Node next;
-   public Node(int value) {
-      this.value = value;
-      next = null;
-   }
+
+class Node {
+
+  public int value;
+  public Node next;
+
+  public Node(int value) {
+    this.value = value;
+    next = null;
+  }
+
+
 }
