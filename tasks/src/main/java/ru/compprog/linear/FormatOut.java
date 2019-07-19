5e9563d53b572450848032fadd058880416a28b5
Artem Luzan <unlimitet2011@yandex.ru>
2019-07-19 21:54:56 +0300

    FormatOutEa

---
+++ tasks/src/main/java/ru/compprog/linear/FormatOut.java
@@ -0,0 +1,10 @@
+package ru.compprog.linear;
+
+public class FormatOut {
+
+  public static void main(String[] args) {
+    System.out.printf("%3d%10s\n%3d%10s\n", 1, "Hello", 2, "World");
+    System.out.printf("%-8.3fЧисло ПИ\n", 3.141593);
+  }
+
+}
