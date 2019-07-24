d04d3c6f9bfe723ef8d68cc6c6a8e6ab28d688a5
Artem Luzan <unlimitet2011@yandex.ru>
2019-07-24 22:32:15 +0300

    MaxThree

---
+++ tasks/src/main/java/ru/compprog/branch/MaksThree.java
@@ -0,0 +1,30 @@
+package ru.compprog.branch;
+
+public class MaksThree {
+
+  public static void main(String[] args) {
+    System.out.println(maxv2(3, 4, 3));
+  }
+
+  public static int max(int a, int b, int c) {
+    int result;
+    if (a > b) {
+      if (a > c) {
+        result = a;
+      } else {
+        result = c;
+      }
+    } if (b > c) {
+      result = b;
+    } else {
+      result = c;
+    }
+    return result;
+  }
+
+  public static int maxv2(int a, int b, int c) {
+    int result = a > b ? a : b;
+    return result > c ? result : c;
+  }
+
+}
