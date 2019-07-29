35e3ac17e9e426db04cce1dbda4b4e622b8e7121
Artem Luzan <unlimitet2011@yandex.ru>
2019-07-29 11:55:44 +0300

    BtKb

---
+++ tasks/src/main/java/ru/compprog/branch/BtKb.java
@@ -0,0 +1,34 @@
+package ru.compprog.branch;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BtKb {
+
+  public static void main(String[] args) {
+    BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+    int a;
+    String spec;
+    try {
+      System.out.println("Введите число");
+      a = Integer.parseInt(bufferedReader.readLine());
+      System.out.println("Введите букву перевода: b или k");
+      spec = bufferedReader.readLine(); //b or k
+      if (spec.equals("b")) {
+        System.out.println(a * 1024);
+      } else if (spec.equals("k")) {
+        System.out.println((double)a / 1024);
+      } else {
+        System.out.println("Неверный ввод");
+      }
+
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+
+
+  }
+
+
+}
