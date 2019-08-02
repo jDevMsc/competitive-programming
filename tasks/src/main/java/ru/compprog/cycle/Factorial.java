7af6a75884f7645fe5eb40c1c65b2fab45a2f280
Artem Luzan <unlimitet2011@yandex.ru>
2019-08-02 22:16:06 +0300

    Factorial

---
+++ tasks/src/main/java/ru/compprog/cycle/Factorial.java
@@ -0,0 +1,23 @@
+package ru.compprog.cycle;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Factorial {
+
+  public static void main(String[] args) throws IOException {
+    BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+    int num = Integer.parseInt(reader.readLine());
+    int factorial = 1;
+
+      for (int i = 1; i <= num; i++) {
+        if(num == 0) {
+          break;
+        }
+        factorial *=i;
+      }
+    System.out.println(factorial);
+  }
+
+}
