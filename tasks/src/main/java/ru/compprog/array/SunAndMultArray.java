1d89dc01003eb0e13bbe7394633ea7b415fab565
Artem Luzan <unlimitet2011@yandex.ru>
2019-08-06 20:56:03 +0300

    SunAndMultArray

---
+++ tasks/src/main/java/ru/compprog/array/SunAndMultArray.java
@@ -0,0 +1,32 @@
+package ru.compprog.array;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class SunAndMultArray {
+
+  public static void main(String[] args) {
+    int[] array = new int[5];
+    int sum=0;
+    int mult=1;
+
+    BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+    for (int i = 0; i < array.length; i++) {
+      try {
+        array[i] = Integer.parseInt(reader.readLine());
+        sum+=array[i];
+        mult*=array[i];
+      }catch (Exception e) {
+        e.printStackTrace();
+      }
+    }
+    for (int i = 0; i < array.length; i++) {
+      System.out.printf("%d \n",array[i]);
+    }
+    System.out.printf("Сумма элементов массива  - %d\n",sum);
+    System.out.printf("Произведение элементов массива  - %d\n",mult);
+
+  }
+
+}
