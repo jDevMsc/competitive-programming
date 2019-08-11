c64e1a22e4ca2b5212b0796ddba4bbc20a5f711c
Artem Luzan <unlimitet2011@yandex.ru>
2019-08-11 21:40:27 +0300

    BubbleSort

--- tasks/src/main/java/ru/compprog/nestloop/MultTable.java
+++ tasks/src/main/java/ru/compprog/nestloop/MultTable.java
@@ -3,6 +3,8 @@ package ru.compprog.nestloop;
 public class MultTable {
 
   public static void main(String[] args) {
+    String string = "je";
+    boolean ye = string.length()==string.toLowerCase().chars().distinct().count();
 
     int[][] multTable = new int[10][10];
     for (int i = 1; i < 10; i++) {
