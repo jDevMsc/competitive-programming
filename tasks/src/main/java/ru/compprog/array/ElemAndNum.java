486b8693baa3c6cfba4e9949d3c80da00ad2b13d
Artem Luzan <unlimitet2011@yandex.ru>
2019-08-11 14:10:02 +0300

    ElemAndNum

---
+++ tasks/src/main/java/ru/compprog/array/ElemAndNum.java
@@ -0,0 +1,25 @@
+package ru.compprog.array;
+
+import java.util.Random;
+
+public class ElemAndNum {
+
+  public static void main(String[] args) {
+    int[] array = new int[10];
+    Random random = new Random();
+    int max = array[0];
+    int ind = 0;
+
+    for (int i = 0; i < array.length; i++) {
+      array[i] = random.nextInt(20);
+      if(array[i]> max) {
+        max = array[i];
+        ind = i; //также можно просто использовать индекс
+      }
+    }
+    System.out.println("Максимальный элемент =" + max);
+    System.out.println("Его индекс =" + ind);
+
+  }
+
+}
