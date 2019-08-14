6000985ffdd43b4698b2d697edf962e7358b0d21
Artem Luzan <unlimitet2011@yandex.ru>
2019-08-14 12:05:34 +0300

    MaxElemEvInd

---
+++ tasks/src/main/java/ru/compprog/array/MaxElemEvInd.java
@@ -0,0 +1,20 @@
+package ru.compprog.array;
+
+/**
+ * in the array find the maximum element with  an even index
+ */
+public class MaxElemEvInd {
+
+  public static void main(String[] args) {
+    int[] array = new int[]{37, 81 ,23, 91 ,66 ,36 ,59,  6, 25 ,57};
+    int max = array[0];
+
+    for (int i = 0; i < array.length; i=i+2) {
+      if(array[i] > max) {
+        max = array[i];
+      }
+    }
+    System.out.println(max);
+
+  }
+}
