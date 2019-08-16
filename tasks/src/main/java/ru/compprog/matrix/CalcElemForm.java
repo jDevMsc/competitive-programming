6feacbf1a3aa5b2b76901977da73dae92d0ffd44
Artem Luzan <unlimitet2011@yandex.ru>
2019-08-16 14:11:36 +0300

    CalcElemForm

---
+++ tasks/src/main/java/ru/compprog/matrix/CalcElemForm.java
@@ -0,0 +1,23 @@
+package ru.compprog.matrix;
+
+public class CalcElemForm {
+
+  public static void main(String[] args) {
+    double[][] matrix = new double[3][4];
+
+    for (int i = 0; i < 3; i++) {
+      for (int j = 0; j < 4; j++) {
+        matrix[i][j] = Math.sin(3*(i+1)+4*(j+1));
+        if(matrix[i][j]<0) {
+          matrix[i][j] = 0;
+        }
+      }
+    }
+    for (int i = 0; i < 3; i++) {
+      for (int j = 0; j < 4; j++) {
+        System.out.println(matrix[i][j]);
+      }
+    }
+  }
+
+}
