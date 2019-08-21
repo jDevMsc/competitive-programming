676bd1d0bd178cdd9b28f125c2edfb06bc8d79a9
Artem Luzan <unlimitet2011@yandex.ru>
2019-08-21 13:34:37 +0300

    (linear) SumMultDigNum

---
+++ tasks/src/main/java/ru/compprog/linear/SumMultDigNum.java
@@ -0,0 +1,32 @@
+package ru.compprog.linear;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/**
+ * The sum and multiply of a digits of a three-digit  number
+ */
+public class SumMultDigNum {
+
+  public static void main(String[] args) throws IOException {
+    BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+    int num = Integer.parseInt(reader.readLine());
+    int a = num/100;
+    int b  = num/10%10;
+    int c = num%10;
+
+    System.out.println("Sum: " + (a+b+c));
+    System.out.println("Mult: " + a*b*c);
+
+    /*цикл
+    int sum = 0;
+    int mult = 1;
+    while(num>0) {
+      sum += num%10;
+      mult *= num%10;
+      num = num/10;
+    }*/
+    //System.out.println("Sum : " + sum + "\nMult : " + mult);
+  }
+}
