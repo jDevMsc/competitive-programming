dab7a5a76ce4cfc381e6733592a8e7ebff13c294
Artem Luzan <unlimitet2011@yandex.ru>
2019-08-30 11:53:22 +0300

    (cycle) ASCIIDisplay

---
+++ tasks/src/main/java/ru/compprog/cycle/ASCIIDisplay.java
@@ -0,0 +1,17 @@
+package ru.compprog.cycle;
+
+/**
+ * ASCII character table display
+ * After every 10ts character, go to a new line
+ */
+public class ASCIIDisplay {
+
+  public static void main(String[] args) {
+    for (int i = 32; i < 128; i++) {
+      System.out.print((char) i + " ");
+      if ((i - 1) % 10 == 0) {
+        System.out.println();
+      }
+    }
+  }
+}
