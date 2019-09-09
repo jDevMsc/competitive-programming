94c81692dfb60dd91f45882fa0f2bb278d994ea3
Artem Luzan <unlimitet2011@yandex.ru>
2019-09-09 21:34:17 +0300

    (array) ArrayRevers vers2

--- tasks/src/main/java/ru/compprog/array/ArrayRevers.java
+++ tasks/src/main/java/ru/compprog/array/ArrayRevers.java
@@ -8,17 +8,25 @@ import java.util.Arrays;
 public class ArrayRevers {
 
   public static void main(String[] args) {
-    int[] arrayStart = new int[]{1,2,3,4,5,6,7};
-    int[] result = new int[7];
+    int[] arrayStart = new int[]{1 ,2 ,3 ,4,5};
+    int[] result = new int[4];
 
-    for (int i = 0; i < arrayStart.length; i++) {
-      result[result.length-i-1] = arrayStart[i];
-    }
-
-    System.out.println(Arrays.toString(result));
+//    for (int i = 0; i < arrayStart.length; i++) {
+//      result[result.length-i-1] = arrayStart[i];
+//    }
+//
+//    System.out.println(Arrays.toString(result));
 
 
     //vers2
+
+    for (int i = 0; i < arrayStart.length / 2; i++) {
+      int temp = arrayStart[i];
+      arrayStart[i] = arrayStart[arrayStart.length - i-1];
+      arrayStart[arrayStart.length - i-1] = temp;
+    }
+    System.out.println(Arrays.toString(arrayStart));
+
 
   }
 }
