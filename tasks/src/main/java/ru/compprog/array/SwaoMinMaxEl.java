b610241d4e355b9ad6f66e78f6772244fe8d328f
Artem Luzan <unlimitet2011@yandex.ru>
2019-09-11 12:09:12 +0300

    (array) SwaoMinMaxEl

---
+++ tasks/src/main/java/ru/compprog/array/SwaoMinMaxEl.java
@@ -0,0 +1,32 @@
+package ru.compprog.array;
+
+import java.util.Arrays;
+
+/**
+ * Swap the minimum and maximum elements
+ */
+public class SwaoMinMaxEl {
+
+  public static void main(String[] args) {
+    int[] array = new int[]{1, 2, 3, 4, 5};
+    int minInd = 0;
+    int maxInd = 0;
+    int temp;
+
+    for (int i = 1; i < array.length; i++) {
+      if (array[i] < array[minInd]) {
+        minInd = i;
+      } else if (array[i] > array[maxInd]) {
+        maxInd = i;
+      }
+    }
+    temp = array[maxInd];
+    array[maxInd] = array[minInd];
+    array[minInd] = temp;
+
+    System.out.println(Arrays.toString(array));
+
+
+  }
+
+}
