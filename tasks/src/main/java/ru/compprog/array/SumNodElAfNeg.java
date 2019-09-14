d9f7e46fc9a721a474a635527929480dda45ec96
Artem Luzan <unlimitet2011@yandex.ru>
2019-09-14 14:22:47 +0300

    (array) SumNodElAfNeg

---
+++ tasks/src/main/java/ru/compprog/array/SumNodElAfNeg.java
@@ -0,0 +1,29 @@
+package ru.compprog.array;
+
+/**
+ * The sum of the modules of the array
+ * elements located after first negative
+ */
+public class SumNodElAfNeg {
+
+  public static void main(String[] args) {
+    int[] array = {5, 3, -1, 8, 0, -6, 1};
+    int sum = 0;
+    int negInd = -1;
+
+    for (int i = 0; i < array.length; i++) {
+      if(array[i] < 0) {
+        negInd = i;
+        break;
+      }
+    }
+    if(negInd > array.length) {
+      System.out.println("В массиве нет отрицательных елементов");
+    } else {
+      for (int i = negInd + 1; i < array.length; i++) {
+        sum += Math.abs(array[i]);
+      }
+      System.out.println(sum);
+    }
+  }
+}
