5de8b10f254836b4303e9854a5ae108cb7bf6490
Artem Luzan <unlimitet2011@yandex.ru>
2019-09-16 16:01:20 +0300

    (matrix) LastElem

---
+++ tasks/src/main/java/ru/compprog/matrix/LastElem.java
@@ -0,0 +1,35 @@
+package ru.compprog.matrix;
+
+import java.util.Random;
+
+/**
+ * The last element of the matrix rows is the sum
+ *  of previous elements of the row
+ */
+public class LastElem {
+
+  public static void main(String[] args) {
+    int[][] matrix = new int[5][4];
+    int sum = 0;
+    Random random  = new Random();
+
+    for (int i = 0; i < matrix.length; i++) {
+      for (int j = 0; j < matrix[0].length; j++) {
+        if (j == matrix[0].length - 1) {
+          matrix[i][j] = sum;
+        } else {
+          matrix[i][j] = random.nextInt(9);
+          sum = sum + matrix[i][j];
+        }
+      }
+      ;
+    }
+    for (int i = 0; i < matrix.length; i++) {
+      for (int j = 0; j < matrix[0].length; j++) {
+        System.out.print(matrix[i][j] + "  ");
+      }
+      System.out.println();
+    }
+
+  }
+}
