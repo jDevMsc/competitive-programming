9bdf80a1a6426ef106de5eb8e13fd88a860b485e
Artem Luzan <unlimitet2011@yandex.ru>
2019-09-19 16:37:49 +0300

    (matrix) ColWithMaxSum

---
+++ tasks/src/main/java/ru/compprog/matrix/ColWithMaxSum.java
@@ -0,0 +1,37 @@
+package ru.compprog.matrix;
+
+import java.util.Random;
+
+/**
+ * Find the matrix column with the  maximum sum of element
+ */
+public class ColWithMaxSum {
+
+  public static void main(String[] args) {
+    int[][] matrix = new int[5][5];
+    Random random = new Random();
+    int col = 0;
+    int sum;
+    int maxSum = 0;
+    for (int i = 0; i < matrix.length; i++) {
+      for (int j = 0; j < matrix[0].length; j++) {
+        matrix[i][j] = random.nextInt(15);
+        System.out.print(matrix[i][j] + "  ");
+      }
+      System.out.println();
+    }
+
+    for (int i = 0; i < matrix.length; i++) {
+      sum = 0;
+
+      for (int j = 0; j < matrix[0].length; j++) {
+        sum += matrix[j][i];
+      }
+      if(maxSum<sum) {
+        maxSum = sum;
+        col = i;
+      }
+    }
+    System.out.println("Столбец с максимальной суммой = " + col);
+  }
+}
