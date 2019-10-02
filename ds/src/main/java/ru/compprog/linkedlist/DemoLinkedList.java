74517a1a3baa8615bb633768ab3d0753e4833d41
Artem Luzan <unlimitet2011@yandex.ru>
2019-10-02 15:00:48 +0300

    (DS) LinkedListView preparation

---
+++ ds/src/main/java/ru/compprog/linkedlist/DemoLinkedList.java
@@ -0,0 +1,43 @@
+package ru.compprog.linkedlist;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class DemoLinkedList {
+
+  public static void main(String[] args) {
+    List<String>list = new LinkedList<>();
+    list.add("a");
+    list.add("b");
+    list.add("c");
+    list.add("d");
+    list.add("e");
+
+    String s = list.get(1);
+    System.out.println(s);
+
+    System.out.println(list.size());
+
+    for (String str :list ) {
+      System.out.println(str);
+    }
+    System.out.println("==========");
+
+    for(Object alist : list) {
+      System.out.println(alist);
+    }
+
+    System.out.println("==========");
+
+    list.remove(1);
+    System.out.println(list.get(1));
+
+    list.remove("d");
+
+    System.out.println(list.size());
+
+    list.set(0,"SET");
+    System.out.println(list.contains("a"));
+
+  }
+}
