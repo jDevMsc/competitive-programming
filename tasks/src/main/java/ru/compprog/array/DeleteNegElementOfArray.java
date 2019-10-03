f738627ebb4363542a3614275b4d4e7b5523a74e
Artem Luzan <unlimitet2011@yandex.ru>
2019-10-03 14:36:38 +0300

    (array) DeleteNegElementOfArray

---
+++ tasks/src/main/java/ru/compprog/array/DeleteNegElementOfArray.java
@@ -0,0 +1,39 @@
+package ru.compprog.array;
+
+import java.util.Arrays;
+import java.util.Random;
+
+/**
+ * Removing negative array elements
+ */
+public class DeleteNegElementOfArray {
+
+  public static void main(String[] args) {
+    Random random = new Random();
+    int[] array = new int[10];
+
+    for (int i = 0; i < array.length; i++) {
+      array[i] = -5 + random.nextInt(5 + 5 + 1);
+    }
+    System.out.println(Arrays.toString(array));
+
+    int i = 0;
+    int lengthAr = 10;
+
+    while (i < lengthAr) {
+      if (array[i] < 0) {
+        lengthAr -= 1;
+        for (int j = i; j < lengthAr; j++) {
+          array[j] = array[j + 1];
+        }
+      } else {
+        i += 1;
+      }
+    }
+
+    for (int j = 0; j < lengthAr; j++) {
+      System.out.print(array[j] + " ");
+    }
+
+  }
+}
