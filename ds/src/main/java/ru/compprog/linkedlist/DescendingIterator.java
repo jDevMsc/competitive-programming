a44aa4381b50170fc7ac43a8b1bc212384122aaf
Artem Luzan <unlimitet2011@yandex.ru>
2019-10-07 15:01:41 +0300

    (DS) LinkedList

---
+++ ds/src/main/java/ru/compprog/linkedlist/DescendingIterator.java
@@ -0,0 +1,8 @@
+package ru.compprog.linkedlist;
+
+import java.util.Iterator;
+
+public interface DescendingIterator<E> {
+
+  Iterator<E> descendingIterator();
+}
---
+++ ds/src/main/java/ru/compprog/linkedlist/Linked.java
@@ -0,0 +1,8 @@
+package ru.compprog.linkedlist;
+
+public interface Linked<E> {
+  void addLast(E e);
+  void addFirst(E e);
+  int size();
+  E getElementByIndex(int counter);
+}
