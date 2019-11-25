437432ce442e580e8b416cb06d0d1d67635d3234
Artem Luzan <unlimitet2011@yandex.ru>
2019-11-25 22:42:47 +0300

    [LinkedList] easy delete

--- ds/src/main/java/ru/compprog/linkedlist/vers4/Node.java
+++ ds/src/main/java/ru/compprog/linkedlist/vers4/Node.java
@@ -19,4 +19,19 @@ public class Node {
     }
     n.next = end;
   }
+
+  Node delete(Node head, int d) {
+    Node n = head;
+    if(n.data == d) {
+      return head.next;
+    }
+    while(n.next != null) {
+      if(n.next.data == d) {
+        n.next = n.next.next;
+        return head;
+      }
+      n = n.next;
+    }
+    return head;
+  }
 }
