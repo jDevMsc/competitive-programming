8b281176abd042ce9ad746690700de4660889800
Artem Luzan <unlimitet2011@yandex.ru>
2019-11-26 21:56:19 +0300

    [Queue] impl remove peel isEmpty

--- ds/src/main/java/ru/compprog/queue/MyQueue.java
+++ ds/src/main/java/ru/compprog/queue/MyQueue.java
@@ -1,5 +1,8 @@
 package ru.compprog.queue;
 
+import java.util.EmptyStackException;
+import java.util.NoSuchElementException;
+
 public class MyQueue<T> {
   private static class QueueNode<T> {
     private T data;
@@ -23,5 +26,24 @@ public class MyQueue<T> {
       first = last;
     }
   }
+
+public T remove() {
+    if(first == null) throw  new NoSuchElementException();
+    T data = first.data;
+    first = first.next;
+    if(first == null) {
+      last = null;
+    }
+    return data;
+}
+
+public T peek() {
+  if(first == null) throw  new EmptyStackException();
+  return  first.data;
+}
+
+public boolean isEmpty() {
+    return first == null;
+}
 
 }
